53d70da456c84f81e50465f06831a6efba0e7720
Gautam Bansal <gautamdevelops@gmail.com>
2024-04-06 22:13:13 +0530

    Corrected the flow

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import About from "../assets/Component/About/About";
+import Campus from "../assets/Component/Campus/Campus";
+import Contact from "../assets/Component/Contact/Contact";
+import Footer from "../assets/Component/Footer/Footer";
+import Hero from "../assets/Component/Hero/Hero";
+import Navbar from "../assets/Component/Navbar/Navbar";
+import Program from "../assets/Component/Program/Program";
+import Testimonials from "../assets/Component/Testimonials/Testimonials";
+import VideoPlayer from "../assets/Component/Video_Player/VideoPlayer";
+import Title from "../assets/Component/title/Title";
+
+function App() {
+  const [playState, setPlayState] = useState(false);
+  return (
+    <>
+      <div className="app">
+        <Navbar />
+        <Hero />
+        <div className="Container">
+          <Title subTitle="OUR PROGRAM" title="What We Offer " />
+          <Program />
+          <About setPlayState={setPlayState} />
+          <Title subTitle="Gallery" title="Campus Photos " />
+          <Campus />
+          <Title subTitle="Testimonials" title="What Student Says " />
+          <Testimonials />
+          <Title subTitle="CONTACT US" title="Get in Touch " />
+          <Contact />
+          <Footer />
+        </div>
+        <VideoPlayer playState={playState} setPlayState={setPlayState} />
+      </div>
+    </>
+  );
+}
+
+export default App;
