3743dfdca405a214dcf80efa26b9462b19d7b94e
Gautam Bansal <gautamdevelops@gmail.com>
2024-04-06 22:18:38 +0530

    Delete node_modules

--- assets/Component/Hero/Hero.tsx
+++ assets/Component/Hero/Hero.tsx
@@ -1,15 +1,20 @@
-import DarkArrow from '../../Images/dark-arrow.png'
-import './Hero.css'
+import DarkArrow from "../../Images/dark-arrow.png";
+import "./Hero.css";
 const Hero = () => {
   return (
-    <div className='hero-container'>
-      <div className='hero-text'>
+    <div className="hero-container">
+      <div className="hero-text">
         <h1>We Ensure Better Education for a Better World</h1>
-        <p>Our cutting-edge cirriculum is designed to empower students with the knowledge, skills and experiences needed to excel in  the dynamic field of education
+        <p>
+          Our cutting-edge cirriculum is designed to empower students with the
+          knowledge, skills and experiences needed to excel in the dynamic field
+          of education
         </p>
-        <button className='btn'>Explore More <img src={DarkArrow} alt="" /></button>
+        <button className="btn">
+          Explore More <img src={DarkArrow} alt="" />
+        </button>
       </div>
     </div>
-  )
-}
-export default Hero
+  );
+};
+export default Hero;
